3a46334b7b1eca544b29095595e69fd22fba07fc
RandallSungcang <122940152+RandallSungcang@users.noreply.github.com>
2023-10-13 23:12:34 -0600

    Inventory GUI sample

--- A_La_Carte/src/main/java/module-info.java
+++ A_La_Carte/src/main/java/module-info.java
@@ -1,10 +1,12 @@
-module com.example.a_la_carte {
+module project.a_la_carte {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.a_la_carte to javafx.fxml;
-    exports com.example.a_la_carte;
+    opens project.a_la_carte to javafx.fxml;
+    exports project.a_la_carte;
+    exports project.a_la_carte.prototype;
+    opens project.a_la_carte.prototype to javafx.fxml;
 }
