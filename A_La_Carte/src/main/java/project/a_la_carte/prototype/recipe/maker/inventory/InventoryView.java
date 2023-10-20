442a42e68329eed1472945b77ae695a4e2204bfb
Randall Sungcang (rts063) <rts063@mail.usask.ca>
2023-10-19 18:46:17 -0600

    Update InventoryView.java

--- A_La_Carte/src/main/java/project/a_la_carte/prototype/recipe/maker/inventory/InventoryView.java
+++ A_La_Carte/src/main/java/project/a_la_carte/prototype/recipe/maker/inventory/InventoryView.java
@@ -2,6 +2,7 @@ package project.a_la_carte.prototype.recipe.maker.inventory;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -9,6 +10,8 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import project.a_la_carte.prototype.ProgramController;
+
+import java.util.List;
 
 public class InventoryView extends StackPane {
     InventoryModel inventoryModel;
@@ -20,13 +23,11 @@ public class InventoryView extends StackPane {
     TextField quantityText;
     Button submit;
     Button mainMenu;
+    ComboBox<String> measurementCBox;
+    Label listLabel;
     public InventoryView(){
         this.setMaxSize(1000,500);
 
-        VBox addVBox = new VBox();
-        listVBox = new VBox();
-
-        addVBox.setPrefSize(300,500);
         addVBox.setStyle("-fx-border-color: black;\n");
         Label addLabel = new Label("Add To Inventory");
         addLabel.setFont(new Font(20));
@@ -51,17 +52,27 @@ public class InventoryView extends StackPane {
         addQuantityHBox.setPrefWidth(300);
         addQuantityHBox.setPadding(new Insets(2,2,2,2));
 
+        //select Measurement method
+        HBox measurementHBox = new HBox();
+        Label measurementSelectLabel = new Label("Select Measurement method");
+        measurementCBox = new ComboBox<>();
+        measurementCBox.getItems().add("Pounds");
+        measurementCBox.getItems().add("Count");
+        measurementHBox.getChildren().addAll(measurementSelectLabel,measurementCBox);
+
+
+
 
         //Will probably have to be a variable as well to connect with controller class
         submit = new Button("Submit");
         mainMenu = new Button("Main Menu");
 
-        addVBox.getChildren().addAll(mainMenu, addLabel,addNameHBox, addQuantityHBox, submit);
+        addVBox.getChildren().addAll(mainMenu, addLabel,addNameHBox, addQuantityHBox,measurementHBox, submit);
         addVBox.setPadding(new Insets(5,5,5,5));
 
         listVBox.setPrefSize(700,500);
         listVBox.setStyle("-fx-border-color: black;\n");
-        Label listLabel = new Label("Inventory");
+        listLabel = new Label("Inventory");
         listLabel.setFont(new Font(20));
 
         listVBox.getChildren().add(listLabel);
@@ -78,6 +89,34 @@ public class InventoryView extends StackPane {
     }
     public void setController(ProgramController controller){
         mainMenu.setOnAction(controller::openStartUpMVC);
+        submit.setOnAction(controller::handleNewIngredient);
 
     }
+
+    public void modelChanged(List<Ingredient> ingredientInventory){
+        //clear text fields
+        nameText.clear();
+        quantityText.clear();
+        measurementCBox.setValue(null);
+        listVBox.getChildren().clear();//redraw list
+        listVBox.getChildren().add(listLabel);
+
+        for(Ingredient i : ingredientInventory){
+            IngredientWidget widget = new IngredientWidget(i);
+            listVBox.getChildren().add(widget.getWidget());
+        }
+    }
+
+    public TextField getNameText() {
+        return nameText;
+    }
+
+    public TextField getQuantityText() {
+        return quantityText;
+    }
+
+    public ComboBox<String> getMeasurementCBox() {
+        return measurementCBox;
+    }
 }
+
