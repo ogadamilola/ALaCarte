05977c61fce591c47b67ef31a8779db730fdd3fb
kcm662@usask.ca <kcm662@usask.ca>
2023-10-19 23:45:12 -0600

    Created Recipie class

--- A_La_Carte/src/main/java/project/a_la_carte/prototype/recipe/maker/inventory/Recipie.java
+++ A_La_Carte/src/main/java/project/a_la_carte/prototype/recipe/maker/inventory/Recipie.java
@@ -1,13 +1,61 @@
 package project.a_la_carte.prototype.recipe.maker.inventory;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Recipie {
 
-    private List<Ingredient> recipieIngredients;
+    private Map<Ingredient,Double> recipieIngredients;
     private String name;
     private String description;
     private String prepInstruction;
     private float price;
     private float prepTime;
+
+    public Recipie(String name){
+        this.name = name;
+        this.recipieIngredients = new HashMap<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setPrepInstruction(String prepInstruction) {
+        this.prepInstruction = prepInstruction;
+    }
+
+    public String getPrepInstruction() {
+        return prepInstruction;
+    }
+
+    public void setPrepTime(float prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public float getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }
