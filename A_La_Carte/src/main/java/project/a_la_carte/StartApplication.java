f84b123656d2645e96899476ee4793f4281610b1
tomi.bamimore <ise652@usask.ca>
2023-11-15 17:38:40 -0600

    Adding app icon and changing app title

--- A_La_Carte/src/main/java/project/a_la_carte/StartApplication.java
+++ A_La_Carte/src/main/java/project/a_la_carte/StartApplication.java
@@ -3,6 +3,8 @@ package project.a_la_carte;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
 //import project.a_la_carte.prototype.StartupMVC; //<- For Prototype un-comment
 import project.a_la_carte.version2.StartupMVC;  //<- For Version2
 
@@ -11,9 +13,12 @@ import java.io.IOException;
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException{
+        Image image = new Image("file:icon.png");
+        stage.getIcons().add(image);
+
         StartupMVC startupMVC = new StartupMVC();
         Scene projectScene = new Scene(startupMVC);
-        stage.setTitle("Prototype Project");
+        stage.setTitle("A La Carte");
         stage.setScene(projectScene);
         stage.show();
     }
